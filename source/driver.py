5d5b46c4bc8ec9ff1d4ae7f3ae6ad259f3143d3f
Reetahan <reetahan@gmail.com>
2023-01-19 18:41:48 -0500

    Further refactoring

--- source/driver.py
+++ source/driver.py
@@ -11,11 +11,59 @@ import copy
 
 def main():
 
-	info = {"conv_thresh": {"vals": np.arange(0.00, 0.11, 0.05), "xlabel": "Conversion Threshold (0% to 10%)"}}
-	other_info = {"conv_thresh": [0.35], "rho_col": [0.7], "init_ratio": [(0.55, 0.45)]}
+	# 2-party BPA
+	info = {"conv_thresh":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Conversion Threshold (0% to 100%)"}, "rho_col":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Rho col (0% to 100%)"}} 
+	other_info = {"conv_thresh": [0.35,0.55,0.75], "rho_col": [0.3,0.5,0.7], "init_ratio":[(0.55,0.45),(0.8,0.2)]}
+
+	fname_prefix='2p_BPA_results/NEW_2P_BPA_simultaneous'
 	y_lab = 'Final Proportions of Parties'
+	model = 'BPA'
+	party_count = 2
 	N = 5000
 	iter_max = 5
+	seed_ct = 30
+
+	'''
+	# 2-party SB
+	info = {"conv_thresh":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Conversion Threshold (0% to 100%)"}, "rho_col":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Rho col (0% to 100%)"},"rho_res":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Rho res (0% to 100%)"}} 
+	other_info = {"conv_thresh": [0.35,0.55,0.75], "rho_col": [0.4,0.6,0.8],"rho_res": [0.1,0.3,0.5], "init_ratio":[(0.55,0.45),(0.8,0.2)]}
+
+	fname_prefix='2p_SB_results/NEW_2P_SB_simultaneous'
+	y_lab = 'Final Proportions of Parties'
+	model = 'SB'
+	party_count = 2
+	N = 5000
+	iter_max = 5
+	seed_ct = 30
+	'''
+
+	'''
+	# 3-party BPA
+	info = {"conv_thresh":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Conversion Threshold (0% to 100%)"}, "rho_col":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Rho col (0% to 100%)"},"rho_res":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Rho res (0% to 100%)"}} 
+	other_info = {"conv_thresh": [0.35,0.55,0.75], "rho_col": [0.4,0.6,0.8], "rho_res": [0.1,0.3,0.5], "init_ratio":[(0.55,0.15,0.3),(0.3,0.15,0.55),(0.4,0.2,0.4),(0.4,0.4,0.2),(0.35,0.25,0.4),(0.4,0.25,0.35)]}
+
+	fname_prefix='3p_BPA_results/NEW_3P_BPA_simultaneous'
+	y_lab = 'Final Proportions of Parties'
+	model = 'BPA'
+	party_count = 3
+	N = 5000
+	iter_max = 5
+	seed_ct = 30
+	'''
+
+	'''
+	# 3-party SB
+	info = {"conv_thresh":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Conversion Threshold (0% to 100%)"}, "rho_col":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Rho col (0% to 100%)"}, "rho_excol":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Rho excol (0% to 100%)"}, "rho_res":{"vals":np.arange(0.00,1.01,0.05), "xlabel": "Rho res (0% to 100%)"}} 
+	other_info = {"conv_thresh": [0.35,0.55,0.75], "rho_col": [0.4,0.6,0.8],"rho_excol": [0.25,0.45,0.65], "rho_res": [0.1,0.3,0.5], "init_ratio":[(0.55,0.15,0.3),(0.3,0.15,0.55),(0.4,0.2,0.4),(0.4,0.4,0.2),(0.35,0.25,0.4),(0.4,0.25,0.35)]}
+
+	fname_prefix='3p_SB_results/NEW_3P_SB_simultaneous'
+	y_lab = 'Final Proportions of Parties'
+	model = 'SB'
+	party_count = 3
+	N = 5000
+	iter_max = 5
+	seed_ct = 30
+	'''
 
 	for question in info:
 
@@ -24,9 +72,9 @@ def main():
 			if(q != question):
 				exp_consts[q] = other_info[q]
 
-		exp = Experiment(model='BPA',party_count=2,fname_prefix='2p_BPA_results/TEST_2P_BPA_simultaneous',test_variable_name=question,
+		exp = Experiment(model=model,party_count=party_count,fname_prefix=fname_prefix,test_variable_name=question,
 			test_variable_range=info[question]['vals'], plot_xlabel=info[question]['xlabel'], constants=exp_consts, plot_ylabel=y_lab,
-			n=N,iter_max=iter_max,seed_ct=30)
+			n=N,iter_max=iter_max,seed_ct=seed_ct)
 		exp.run_experiment()
 
 		p = Plot(fnames=exp.fnames, titles=exp.titles,party_count=2,var_range=info[question]['vals'],xlabel=info[question]['xlabel'],ylabel=y_lab)
--- source/plots.py
+++ source/plots.py
@@ -11,6 +11,11 @@ class Plot:
 		self.xlabel = xlabel
 		self.ylabel = ylabel
 
+		print(self.file_list)
+		print(self.title_list)
+		print(self.xlabel)
+		print(self.ylabel)
+
 	def generate_plot(self,show):
 		for i in range(len(self.file_list)):
 
@@ -41,3 +46,79 @@ class Plot:
 			if(show):
 				plt.show()
 				plt.clf()
+
+'''
+def main():
+	#file_list = ['3p_BPA_results/' + x for x in os.listdir('3p_BPA_results')]
+	file_list = ['2p_BPA_results/' + x for x in os.listdir('2p_BPA_results')]
+	#file_list = ['2p_SB_results/' + x for x in os.listdir('2p_SB_results')]
+	#file_list = ['serial_2p_BPA_results/' + x for x in os.listdir('serial_2p_BPA_results')]
+	#file_list = ['serial_2p_SB_results/' + x for x in os.listdir('serial_2p_SB_results')]
+
+	for file_ in file_list:
+		fparts = file_.split('_')
+		xlabel = 'Conversion Threshold'
+		if(fparts[5] == 'RHORES'):
+			xlabel = 'Rho res'
+		if(fparts[5] == 'RHOCOL'):
+			xlabel = 'Rho col'
+		ratio = fparts[8]
+		rhocol = None 
+		rhores = None 
+		conv_thresh = None 
+		if(fparts[6] == 'rc'):
+			rhocol = fparts[7]
+			ratio = fparts[9].split('.txt')[0]
+		if(fparts[9] == 'rhocol'):
+			rhocol = fparts[10]
+		if(fparts[9] == 'rhores'):
+			rhores = fparts[10]
+		if(fparts[9] == 'convthresh'):
+			conv_thresh = fparts[10].split('.txt')[0]
+		if(len(fparts) > 11 and fparts[11] == 'rhocol'):
+			rhocol = fparts[12].split('.')[0]
+		if(len(fparts) > 11 and fparts[11] == 'rhores'):
+			rhores = fparts[12].split('.')[0]
+		if(len(fparts) > 11 and fparts[11] == 'convthresh'):
+			conv_thresh = fparts[12].split('.')[0]
+		
+		#title = "3-party BPA | " + str(xlabel) + " | " + ratio + " | "
+		title = "2-party BPA | " + str(xlabel) + " | " + ratio + " | "
+		#title = "2-party SB | " + str(xlabel) + " | " + ratio + " | "
+		#title = "Serial 2-party BPA | " + str(xlabel) + " | " + ratio + " | "
+		#title = "Serial 2-party SB | " + str(xlabel) + " | " + ratio + " | "
+		if(rhocol != None):
+			title += ' rho_col: ' + str(rhocol)
+		if(rhores != None):
+			title += ' rho_res: ' + str(rhores)
+		if(conv_thresh != None):
+			title += ' conv_thresh: ' + str(conv_thresh)
+
+		try:
+			f = open(file_,"r")
+			lines = f.readlines()
+			f.close()
+		except:
+			continue
+
+		x = np.arange(0,101,5)
+		r = [float(lines[i]) for i in range(24,45)]
+		b = [float(lines[i]) for i in range(46,67)]
+		#g = [float(lines[i]) for i in range(68,89)]
+
+		plt.plot(x,r,color='red',label='Red Party')
+		plt.plot(x,b,color='blue',label='Blue Party')
+		#plt.plot(x,g,color='green', label = 'Green Party')
+		plt.xlabel(xlabel)
+		plt.title(title)
+		plt.legend()
+		new_fig_name = file_.split('.txt')[0] + '.png'
+		plt.savefig(new_fig_name)
+		plt.clf()
+		#plt.show()
+'''
+
+
+
+if __name__ == '__main__':
+	main()
